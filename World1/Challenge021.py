0beedbc33f1cfe2676646b0cfcbd3576a6acc2d8
Sirlene Andreis <andreissirlene@gmail.com>
2020-08-27 21:35:37 +0100

    Solve issue in Challenge 021

--- World1/Challenge021.py
+++ World1/Challenge021.py
@@ -1,7 +1,6 @@
-
-import pygame 
-
-mixer.init()
-mixer.music.load('ex01.mp3')
-mixer.music.play()
-while mixer.music.get_busy() == True: continue
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('Moana.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
