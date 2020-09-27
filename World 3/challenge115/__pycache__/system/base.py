49e70d5fd333127d5f5631bb499daebcb93aa928
Sirlene Andreis <andreissirlene@gmail.com>
2020-09-27 12:37:21 +0100

    Challenge 115 completed

--- World 3/challenge115/__pycache__/system/base.py
+++ World 3/challenge115/__pycache__/system/base.py
@@ -31,3 +31,35 @@ def menu(lista):
     line()
     opc = leiaInt('Your option: ')
     return opc
+
+
+def see():
+    while True:
+        try:
+            file = open('profile.txt', 'r')  # open is a internal function of the python program
+            txt = file.read()
+            for profile in txt:
+                print(profile, end='')
+            file.close()
+            break
+        except FileNotFoundError:
+            print('File do not exist\nBut was created!')
+            file = open('profile.txt','w+')
+            file.close()
+            break
+        return file
+
+def register():
+    file = open('profile.txt','a')
+    while True:
+        try:
+            name = str(input('Name: ')).upper().strip()
+            age = int(input('Age: '))
+        except:
+            print('Type a valid value')
+        else:
+            file.write(f'{name:<20}{age:>15} anos')
+            file.write('\n')
+            file.close()
+            break
+
--- World 3/challenge115/__pycache__/system/program.py
+++ World 3/challenge115/__pycache__/system/program.py
@@ -1,14 +1,18 @@
 import base
 
 while True:
-    answer = base.menu (['Check Registered profiles', 'Register People', 'Exit System'])
+    answer = base.menu (['Check Registered People', 'Register People', 'Exit System'])
     if answer == 1:
-        base.title('Option 1')
+        base.title('REGISTERED PEOPLE')
+        base.see()
+
     elif answer == 2:
-        base.title('Option 2')  
+        base.title('NEW REGISTER')
+        base.register()
     elif answer == 3:
-        base.title('Living the system... Bye!')
+        base.title('Leaving the system... Bye!')
         break
     else:
         print('ERROR: please type a valid option!')
     print()
+    
